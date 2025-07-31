21fef908872fb45bb6f7b2473e7479659ccd9773
Applepig Wu <applepig81@gmail.com>
2025-07-31 15:34:44 +0800

    add favicon

--- build.js
+++ build.js
@@ -12,8 +12,24 @@ console.log('資料轉換完成。');
 const srcHtmlPath = path.join(__dirname, 'src', 'index.html');
 const destHtmlPath = path.join(__dirname, 'dist', 'index.html');
 
-console.log('複製 HTML 檔案...');
-fs.symlinkSync(srcHtmlPath, destHtmlPath);
-console.log('HTML 檔案複製完成。');
+if (!fs.existsSync(destHtmlPath)) {
+    console.log('複製 HTML 檔案...');
+    fs.symlinkSync(srcHtmlPath, destHtmlPath, 'dir');
+    console.log('HTML 檔案複製完成。');
+} else {
+    console.log('HTML 檔案已存在，跳過複製。');
+}
+
+// 步驟 3: 複製 src/icon 到 dist/icon
+const srcIconPath = path.join(__dirname, 'src', 'icon');
+const destIconPath = path.join(__dirname, 'dist', 'icon');
+
+if (!fs.existsSync(destIconPath)) {
+    console.log('複製 icon 檔案...');
+    fs.symlinkSync(srcIconPath, destIconPath, 'dir');
+    console.log('icon 檔案複製完成。');
+} else {
+    console.log('icon 檔案已存在，跳過複製。');
+}
 
 console.log('建置成功！');
